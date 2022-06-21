373f4c5e1712efcce1ca6ecda1c2eba13d7f9c78
sambweber <42612830+sambweber@users.noreply.github.com>
2022-06-21 19:13:23 +0100

    Update MoniTool.R

--- R/MoniTool.R
+++ R/MoniTool.R
@@ -11,16 +11,16 @@
 #' May also optionally have columns called 'beach' to differentiate sites, and 'season' to name nesting 
 #' seasons. If 'season' is missing it will be guessed from the dates provided.  
 
-#' @param max.days is a number giving the maximum number of days over which the counted
-#' activities may have occurred. So `max.days = 1` means all activities occurred on the
+#' @param max.days is a number giving the maximum number of days over which the
+#' activities counted in each survey may have occurred. So `max.days = 1` means all activities occurred on the
 #' the night before survey was done (for morning counts). For `max.days > 1` counted
 #' activities are assumed to have occurred over all days since the previous count, up to 
 #' a maximum of `date - max.days`. This requires you to make some informed assumption about 
 #' how many days activities remain visible for. For more complex monitoring designs it is
 #' also possible to include a column in `data` called `datestart` which contains the earliest dates
 #' when counts in each survey could have occurred. For example, if tracks were obliterated two days
-#' before the survey on some occassions, then `datestart` could be set to two days before the
-#' `date - 2 days` for these observations and to `max.days` for the remainder.
+#' before the survey on some occassions, then `datestart` could be set to 
+#' `date - 2 days` for these observations and to `date - max.days` for the remainder.
 #' 
 #' @season_start is a character vector giving a nominal start date for the nesting season. The value
 #' is used to convert dates in `data` to the number of days since the start of the nesting season. 
@@ -29,12 +29,16 @@
 #' @param min.obs is a single numeric giving the minimum number of observations that are needed 
 #' for a given beach in a given season to fit a model. Beaches with `< min.obs` counts will 
 #' be removed from the data
+#'
+#' @param sites.together A logical indicating whether counts on beaches in each season should be modelled together.
+#' This is intended to allow users to estimate some joint parameters across sites (e.g. peak date) but the
+#' initialization process is currently too slow with multiple beaches so it is recommended to leave sites.together = F.
 
 require(magrittr)
 require(dplyr)
 require(lubridate)
 
-MT_prep = function(data, season.start, max.days = 1, min.obs = 10){
+MT_prep = function(data, season.start, max.days = 1, min.obs = 10, sites.together = F){
   
   if(!has_name(data,'date')) stop('Data must contain a column called date')
   if(!is(data$date,'Date')){
@@ -71,11 +75,14 @@ MT_prep = function(data, season.start, max.days = 1, min.obs = 10){
     print(data.frame(distinct(subset(data,too_few),season,beach)),row.names = F)
   }
   
+  nest.vars = c('season','reference_data')
+  if(sites.separate) nest.vars = c(nest.vars,'beach')
+  
   # Prep final output
   subset(data,!too_few) %>%
+    droplevels() %>%
     dplyr::select(any_of(c('season','beach','day','datestart','date','window')),everything(),-too_few) %>%
-    nest(data = -c(season,reference_date)) %>%
-    mutate(data = map(data, droplevels))
+    nest(data = -nest.vars) 
   
 }
 
