f0cc8b66625e2239280ec29fcfb72cab4c31bb38
sambweber <42612830+sambweber@users.noreply.github.com>
2022-06-23 19:37:48 +0100

    Update MoniTool.R

--- R/MoniTool.R
+++ R/MoniTool.R
@@ -85,7 +85,7 @@ MT_prep = function(data, reference.date, max.days = 1, min.obs = 10, sites.toget
   # Prep final output
   data %<>% subset(!too_few) %>%
     droplevels() %>%
-    dplyr::select(any_of(c('season','beach','day','datestart','date','window')),everything(),-too_few) %>%
+    dplyr::select(any_of(c('season','reference_date','beach','day','datestart','date','window')),everything(),-too_few) %>%
     nest(data = -any_of(nest.vars)) 
   
   class(data) <- c('MT_df',class(data))
