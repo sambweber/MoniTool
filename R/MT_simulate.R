498a1a949e58e24ca7e03d1992a3cac0f6589580
sambweber <42612830+sambweber@users.noreply.github.com>
2023-09-14 15:16:21 +0100

    Update MT_simulate.R

--- R/MT_simulate.R
+++ R/MT_simulate.R
@@ -1,12 +1,12 @@
-simulate_season = function(N,phenology,theta){
+simulate_season = function(phenology,days,N,theta){
   
   if(is(phenology,'MTpred')){
-    if(has_name(phenology,'mu')) p = phenology$mu else {
-      stop("'phenology' should be a numeric vector of weights for each day in the time series or an 'MTpred' object")
+      predict(phenology,samples=1,days = days)$mu
+      stop("'phenology' should be a numeric vector of weights for each day in 'days' or an 'MTpred' object")
     }
   } else {p = phenology}
   
-  t  = length(p)
+  t  = length(days)
   w  = rgamma(t,shape=theta,scale=1/theta)
   mu = N*p
   Y = table(factor(sample(t, N, replace=TRUE,prob=p*w), levels=1:t)) 
