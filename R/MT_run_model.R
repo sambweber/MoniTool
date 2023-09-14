56f1f6fb25ed2ebea3fb81599b8ea14e44f77e92
sambweber <42612830+sambweber@users.noreply.github.com>
2023-09-14 16:19:00 +0100

    Update MT_run_model.R

--- R/MT_run_model.R
+++ R/MT_run_model.R
@@ -264,7 +264,7 @@ fit = runMCMC(cbuilt, nchains = nchains, niter = niter, nburnin = nburnin, inits
 
 class(fit) <- c('MTfit',class(fit))
 attr(fit,'y.names') <- model$origData$y.names
-attr(fit,'data') <- dplyr::select(data,day,window,any_of('beach','nests','activities'))
+attr(fit,'data') <- dplyr::select(data,day,window,any_of(c('beach',model$origData$y.names)))
 
 return(fit)
 
