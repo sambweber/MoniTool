6f98ea6178d16598719faa9138f3da8055c2b271
sambweber <42612830+sambweber@users.noreply.github.com>
2023-09-15 14:55:19 +0100

    Update MT_simulate.R

--- R/MT_simulate.R
+++ R/MT_simulate.R
@@ -166,8 +166,20 @@ simulate_phenology.MT_df = function(phenology,days,total,n.sims){
   
 }
 
+
+# -------------------------------------------------------------------------------------------------------------
+# Plotting method for MTsim objects
+# --------------------------------------------------------------------------------------------------------------
+
+# Optionally highlights points that have been retained by a monitoring protocol applied using MT_protocol
+
 plot.MTsim = function(x){
-ggplot(a,aes(x = day)) + geom_point(aes(y=sim)) + geom_line(aes(y=mu), colour = 'blue') + facet_wrap(~.sim)
+  
+p = ggplot(x,aes(x = day, y=sim)) + geom_point()) + geom_line(aes(y=mu), colour = 'blue') + facet_wrap(~.sim)
+
+if(has_name(x,'include')){ p = p + geom_point(data = x[include,],colour='yellow',size=2) }
+
+return(p)
 }
 
 
