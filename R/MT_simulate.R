9c3252ba6cd6f444b6f270f0eb33be7ca7112775
sambweber <42612830+sambweber@users.noreply.github.com>
2024-01-08 16:29:47 +0000

    Update MT_simulate.R

--- R/MT_simulate.R
+++ R/MT_simulate.R
@@ -199,7 +199,8 @@ if(has_name(MTsim,'.include')) MTsim <- subset(MTsim,.include)
 x = dplyr::select(MTsim,.sim,day,y.var,sim) %>% 
 spread('y.var','sim') %>%
 mutate(window = 1) %>%
-nest(data = -.sim)
+nest(data = -.sim) %>%
+mutate(.total =  attr(MTsim,'.totals'),.after=.sim)
 
 class(x) <- c('MT_df',class(x)[!class(x) %in% c('MTsim','MTpred')])
 
