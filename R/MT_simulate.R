1f67c75c3ee853d8192a611c2bc544ed6592fa17
sambweber <42612830+sambweber@users.noreply.github.com>
2024-01-19 15:41:12 +0000

    Update MT_simulate.R

--- R/MT_simulate.R
+++ R/MT_simulate.R
@@ -235,8 +235,6 @@ simulate_phenology.MT_df = function(phenology,days,total,n.sims,shift=0){
 
 plot.MTsim = function(x){
 
-x = unnest(x,data)
-  
 p = ggplot(x,aes(x = day, y=N.sim)) + geom_point() + geom_line(aes(y=mu), colour = 'blue') + facet_wrap(~.sim)
 
 if(has_name(x,'.include')){ p = p + geom_point(data = subset(x,.include),colour='yellow',size=2) }
